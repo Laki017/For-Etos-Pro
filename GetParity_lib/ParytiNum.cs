c9a0c3c94680e4cf178dbb3663db97ec6a433f1a
Laki Lak <admin_xxxx@mail.ru>
2021-07-02 10:36:28 +0300

    Добавьте файлы проекта.

---
+++ GetParity_lib/ParytiNum.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GetParityNum_lib
+{
+    public static class ParytiNum
+    {
+        public static bool IsParytiNum(this int num) => (num & 1) == 0;
+
+        public static bool IsParytiNumber(this int num) => num % 2 > 0 ? false : true;
+    }
+}
